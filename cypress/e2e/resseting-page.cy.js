dc757a2b8095c8c624faa894b3231df8ab637ab0
Mateusz Kubicki <m.kubicki1205@gmail.com>
2022-07-14 21:17:11 +0200

    resseting-page-tests

---
+++ cypress/e2e/resseting-page.cy.js
@@ -0,0 +1,25 @@
+describe('resseting-page', () => {
+    beforeEach(() => {
+        cy.login('standard_user', 'secret_sauce')
+    })
+
+    it('T-01 (Verify reset app state button reset items in cart))', () => {
+        cy.addProduct()
+        cy.get('[id="react-burger-menu-btn"]').click()
+        cy.get('[id="reset_sidebar_link"]').click()
+        cy.get('.shopping_cart_badge').should('not.exist')
+        cy.get('.shopping_cart_link').click()
+        cy.get('.cart_item').should('not.exist')
+      })
+
+      it('T-02 (Verify reset app state button reset sorting options on product list)', () => {
+        cy.get('[data-test="product_sort_container"]').select('za')
+        cy.get('[id="react-burger-menu-btn"]').click()
+        cy.get('[id="reset_sidebar_link"]').click()
+        cy.get('[data-test="product_sort_container"]')
+        cy.get('.inventory_item_name').first().should('have.text', 'Sauce Labs Backpack')
+        
+      })
+
+      
+})
