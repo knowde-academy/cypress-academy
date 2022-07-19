3e6465ad608e2ed7521d5a18dbb54f9935fb434c
MichalQAmaster <michal17@poczta.onet.eu>
2022-07-20 00:08:39 +0200

    Add Page Objects and Data test file

---
+++ cypress/Datatest.js
@@ -0,0 +1,38 @@
+const Data_test = {
+    item_3: {
+            name: 'Test.allTheThings() T-Shirt (Red)',
+            url:'https://www.saucedemo.com/inventory-item.html?id=3',
+            src_img: '/static/media/red-tatt-1200x1500.e32b4ef9.jpg'
+    },
+    item_2: {
+            name: 'Sauce Labs Onesie',
+            url:'https://www.saucedemo.com/inventory-item.html?id=2',
+            src_img: '/static/media/red-onesie-1200x1500.1b15e1fa.jpg'
+    },
+    item_5: {
+            name: 'Sauce Labs Fleece Jacket',
+            url:'https://www.saucedemo.com/inventory-item.html?id=5',
+            src_img: '/static/media/sauce-pullover-1200x1500.439fc934.jpg'
+    },
+    item_1: {
+            name: 'Sauce Labs Bolt T-Shirt',
+            url:'https://www.saucedemo.com/inventory-item.html?id=1',
+            src_img: '/static/media/bolt-shirt-1200x1500.c0dae290.jpg'
+    },
+    item_0: {
+            name: 'Sauce Labs Bike Light',
+            url:'https://www.saucedemo.com/inventory-item.html?id=0',
+            src_img: '/static/media/bike-light-1200x1500.a0c9caae.jpg'
+    },
+    item_4: {
+            name: 'Sauce Labs Backpack',
+            url:'https://www.saucedemo.com/inventory-item.html?id=4',
+            src_img: '/static/media/sauce-backpack-1200x1500.34e7aa42.jpg'
+    },
+}
+
+const img_numbers_board =[0, 1, 2, 3, 4, 5]
+
+
+export {Data_test}
+export {img_numbers_board}
