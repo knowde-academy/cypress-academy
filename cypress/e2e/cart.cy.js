4bb9d11896f4230f2adfc1aaea79f3206ee7f2c6
Michał Zajdel <zajmich@gmail.com>
2022-07-20 01:14:17 +0200

    fixed cart bagde

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,16 +1,10 @@
 describe("Cart", () => {
-  // before each test visit page and log in (correct user)
   beforeEach(() => {
     cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
-    // let cookie1 = document.cookiel;
-    cy.getCookies()
-      .should("have.length", 1)
-      .then((cookies) => {
-        expect(cookies[0]).to.have.property("name", "session-username");
-      });
+    cy.getCookies("session-username");
   });
 
   it("Viewing available products", () => {
@@ -19,13 +13,13 @@ describe("Cart", () => {
 
   it("Adding to cart from list view", () => {
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    cy.get(".shopping_cart_link").click();
+    cy.get(".shopping_cart_badge").contains("1").should("be.visible");
   });
 
   it("Add to cart from product view", () => {
     cy.get("#item_0_img_link > .inventory_item_img").click();
     cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-    cy.get(".shopping_cart_link").click();
+    cy.get(".shopping_cart_badge").contains("1").should("be.visible");
   });
 
   it("Removing products from a cart", () => {
@@ -33,6 +27,7 @@ describe("Cart", () => {
     cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
     cy.get(".shopping_cart_link").click();
     cy.get('[data-test="remove-sauce-labs-bike-light"]').click();
+    cy.get(".shopping_cart_badge").should("not.exist");
   });
 
   it("Browsing products in a cart", () => {
