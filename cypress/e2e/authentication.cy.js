f4ec3b22a5194c437a961eca40cb6278bd1dc578
Michał Zajdel <zajmich@gmail.com>
2022-07-19 13:54:01 +0200

    CR fix

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -1,46 +1,57 @@
 describe("Authentication", () => {
-  it("Login with standard user", () => {
+  beforeEach(() => {
     cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
+  });
+  it("Login with standard user", () => {
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
+    cy.getCookie("standard_user");
   });
 
   it("Login with locked out user", () => {
-    cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("locked_out_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]')
+      .contains("Epic sadface: Sorry, this user has been locked out.")
+      .should("be.visible");
+    cy.getCookie("locked_out_user");
   });
 
   it("Login with problem user", () => {
-    cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("problem_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
+    cy.getCookie("problem_user");
   });
 
   it("Login with performance glitch user", () => {
-    cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("performance_glitch_user");
     cy.get('[data-test="password"]').type("secret_sauce");
-    cy.get('[data-test="login-button"]').click();
+    cy.getCookie("performance_glitch_user");
   });
 
   it("Fields have not beeen completed", () => {
-    cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
     cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]')
+      .contains("Epic sadface: Username is required")
+      .should("be.visible");
   });
 
-  it("Field 'password' hah not beeen completed", () => {
-    cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
+  it("Field 'password' has not beeen completed", () => {
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]')
+      .contains("Epic sadface: Password is required")
+      .should("be.visible");
   });
 
   it("Field 'login' has not beeen completed", () => {
-    cy.visit("/").url().should("eq", "https://www.saucedemo.com/");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]')
+      .contains("Epic sadface: Username is required")
+      .should("be.visible");
   });
 });
